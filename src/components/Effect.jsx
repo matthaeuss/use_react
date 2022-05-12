4d268b1fd4d06f5f9f9f1fdd923dd257606951c6
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-12 20:00:22 +0200

    components created

---
+++ src/components/Effect.jsx
@@ -0,0 +1,20 @@
+import {useEffect, useState} from "react";
+
+function EffectComponent(){
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+            const timer = setTimeout(() => {
+                setTime(new Date())
+            }, 1000)
+            return () => {
+                clearTimeout(timer)
+            }
+        })
+
+    return (
+        <h2>useEffect example: {time.toLocaleTimeString()}</h2>
+    )
+}
+
+export default EffectComponent;
